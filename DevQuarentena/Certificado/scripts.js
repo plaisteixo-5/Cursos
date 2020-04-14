4f44a70723e710ef38f79a1213116b44d6f2b388
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-14 14:45:13 -0300

    Atualizei alguns dos projetos e finalizei o curriculo

--- DevQuarentena/Certificado/scripts.js
+++ DevQuarentena/Certificado/scripts.js
@@ -16,18 +16,24 @@ function gerar(){
         "https://codepen.io/plaisteixo-5/pen/dyoLZBo",
         "https://codepen.io/plaisteixo-5/pen/jOPjNqJ",
         "https://codepen.io/plaisteixo-5/pen/MWwROBX",
-        "https://codepen.io/plaisteixo-5/pen/VwvYVNz"
+        "https://codepen.io/plaisteixo-5/pen/VwvYVNz",
+        "https://codepen.io/plaisteixo-5/pen/WNQQgzm",
+        "https://codepen.io/plaisteixo-5/pen/qBOOMym",
+        "https://codepen.io/plaisteixo-5/pen/oNjjPKp"
     ];
     titulos = [
         "Sorteio",
         "Curriculo",
         "Calculadora de Corrida",
-        "Botão de Like"
+        "Botão de Like",
+        "Olímpiadas",
+        "Ficha de Dados",
+        "Calculadora IMC"
     ]//listaL e titulos são variáveis vetoriais que comportam todos os nomes e links que quiser colocar
 
     cont = 0;
 
-    while(cont < 4){
+    while(cont < 7){
         listaLink = listaLink + `
         <li>
             <a target="_blank" href="${listaL[cont]}">${titulos[cont]}
