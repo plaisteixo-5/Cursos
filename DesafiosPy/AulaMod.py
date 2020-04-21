e9e17cb24892581736e712898093b653410105eb
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-20 22:57:13 -0300

    Aula de modularização de códigos

---
+++ DesafiosPy/AulaMod.py
@@ -0,0 +1,6 @@
+import teste
+
+num = int(input('Digite um valor: '))
+fat = teste.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro é {teste.dobro(num)} e o triplo é {teste.triplo(num)}')
