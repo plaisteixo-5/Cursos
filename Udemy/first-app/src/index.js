7f1e20474b91eb3c72d4501224d2dd6bdad152a4
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-05-22 16:45:22 -0300

    Projeto inicial que vou realizar os testes no curso da Udemy

---
+++ Udemy/first-app/src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './Index.css'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
