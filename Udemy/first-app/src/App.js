dcab6affef71e379b6d639a2d3f960eb518d7914
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-05-22 16:55:44 -0300

    Implementando um código JSX e vendo como é o funcionamento dele no Java script real

--- Udemy/first-app/src/App.js
+++ Udemy/first-app/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
 import './App.css'
 
 class App extends Component {
@@ -8,6 +7,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, im a React App</h1>
             </div>
+            // React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Hi, im a React App he he'))    
         )
     }
 }
