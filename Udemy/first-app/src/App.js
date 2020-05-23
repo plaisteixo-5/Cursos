5b7d2f8714f1874358b36d8ac6eb80f5670d5e39
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-05-22 22:07:53 -0300

    Using the props.child

--- Udemy/first-app/src/App.js
+++ Udemy/first-app/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, im a React App</h1>
                 <p>This is really working!</p>
-                <Person name="Fon" age="19" />
+                <Person name="Fon" age="19" >Como pão</Person>
                 <Person name="Manu" age="29"/>
                 <Person name="Mario" age="22" />
             </div>
