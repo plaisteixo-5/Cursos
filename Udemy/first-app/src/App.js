c352ea273e35edb772328036337e01d800fcfa9d
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-05-24 16:08:11 -0300

    Pass a reference to a method and component using two differents ways
    In button i use a arrow function to return function "this.switchNameHandler("Mariana")"
    And in propriety "click" i pass "this.switchNameHandler.bind(this, 'Mariano!')".
    This is two ways to pass a reference to a function in a class.

--- Udemy/first-app/src/App.js
+++ Udemy/first-app/src/App.js
@@ -1,42 +1,49 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const 
-App = () =>  {
+class App extends Component  {
 
-  const [ personsState, setPersonsState ] = useState({
+  state = {
     persons: [
       { name:"Carol", age:22 },
       { name:"Fon", age:19 },
       { name:"Marcoline", age:21 }
-    ]
-  });
+    ],
+    otherState: 'Some value'
+  }
 
-  const [ otherState, setOtherState] = useState('some other value');
-
-  const switchNameHandler = () => {
+  switchNameHandler = ( newName ) => {
     // DONT DO THIS: this.state.persons[0].name = "Felipe Fontenele"
-    setPersonsState(
+    this.setState(
       {
         persons: [
-          { name:"Maria", age:22 },
+          { name: newName, age:22 },
           { name:"Fon", age:19 },
           { name:"Marcoline", age:21 }
         ]
     });
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person age={ personsState.persons[0].age } name={ personsState.persons[0].name } />
-      <Person age={ personsState.persons[1].age } name={ personsState.persons[1].name }>Come pamonha</Person>
-      <Person age={ personsState.persons[2].age } name={ personsState.persons[2].name } />
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={() => { this.switchNameHandler('Mariana') }}>Switch name</button>
+        <Person 
+          age={ this.state.persons[0].age } 
+          name={ this.state.persons[0].name } />
+        <Person 
+          age={ this.state.persons[1].age } 
+          name={ this.state.persons[1].name }
+          click={ this.switchNameHandler.bind(this,'Mariano!') }
+          >Come pamonha</Person>
+        <Person 
+          age={ this.state.persons[2].age } 
+          name={ this.state.persons[2].name } />
+      </div>
+  )}
 }
 
 export default App;
