8d678ddd26138dc516a00f88a0ed169a35bd4009
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-05-24 21:20:22 -0300

    Using a style in css file

--- Udemy/first-app/src/App.js
+++ Udemy/first-app/src/App.js
@@ -35,11 +35,22 @@ class App extends Component  {
   }
 
   render(){
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      paddin: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => { this.switchNameHandler('Mariana') }}>Switch name</button>
+        <button
+        style={style} 
+        onClick={() => { this.switchNameHandler('Mariana') }}>Switch name</button>
         <Person 
           age={ this.state.persons[0].age } 
           name={ this.state.persons[0].name } />
