7cb156c8fb2dfa3264f99352ba7f61ea5109d536
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-06-03 23:18:17 -0300

    Aula 2

--- RocketSeat/Next Level Week/server/src/server.ts
+++ RocketSeat/Next Level Week/server/src/server.ts
@@ -1,16 +1,14 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes'
+import cors from 'cors'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Mano q isso');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    response.json( [
-        'Marcelo',
-        'Mario',
-        'Marianna',
-        'Fontenele'
-    ] );
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
