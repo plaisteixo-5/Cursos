71d6fcc2716b51833073b661dd20f0129ac81701
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-07-08 23:55:06 -0300

    Class about content variable

--- Udemy/react-complete-guide/src/Components/Expenses/Expenses.js
+++ Udemy/react-complete-guide/src/Components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,18 +17,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if(filteredExpenses.length > 0) expensesContent = filteredExpenses.map((item) => (
-    <ExpenseItem
-      selected={filteredYear}
-      key={item.id}
-      title={item.title}
-      amount={item.amount}
-      date={item.date}
-    />
-));
-
   return (
     <div>
       <Card className="expenses">
@@ -35,7 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        { expensesContent }
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
