8f545fb9136cd99e11de328fee29b42aeb71bf72
Felipe <felipefontenele1317@gmail.com>
2021-07-16 13:50:59 -0300

    N sei mano, to cansado

--- Udemy/01-project/src/Components/Users/AddUser.js
+++ Udemy/01-project/src/Components/Users/AddUser.js
@@ -12,11 +12,18 @@ const AddUser = (props) => {
     event.preventDefault();
 
     setUsers((user) => {
-      return [...users, { name: userName, age: userAge }];
+      return [
+        ...users,
+        { id: Math.random().toString(), name: userName, age: userAge },
+      ];
     });
+    setUserAge('');
+    setUserName('');
   };
 
-  const [users, setUsers] = useState([{ name: "Felipe", age: 20 }]);
+  const [users, setUsers] = useState([
+    { id: Math.random().toString(), name: "Felipe", age: 20 },
+  ]);
 
   const userNameChangeHandler = (event) => {
     setUserName(event.target.value);
@@ -26,20 +33,43 @@ const AddUser = (props) => {
     setUserAge(event.target.value);
   };
 
+  const deleteItemHandler = (userId) => {
+    setUsers(() => {
+      const updatedUser = users.filter((user) => user.id !== userId);
+      return updatedUser;
+    });
+  };
+
   return (
     <>
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">User Name</label>
-          <input id="username" type="text" onChange={userNameChangeHandler} />
+          <input
+            value={userName}
+            id="username"
+            type="text"
+            onChange={userNameChangeHandler}
+          />
           <label htmlFor="userage">Age</label>
-          <input id="userage" type="number" onChange={userAgeChangeHandler} />
+          <input
+            value={userAge}
+            id="userage"
+            type="number"
+            onChange={userAgeChangeHandler}
+          />
           <button type="submit">Add User</button>
         </form>
       </Card>
       <div>
         {users.map((user) => (
-          <User userName={user.name} userAge={user.age} />
+          <User
+            onDelete={deleteItemHandler}
+            id={user.id}
+            key={user.id}
+            userName={user.name}
+            userAge={user.age}
+          />
         ))}
       </div>
     </>
--- Udemy/01-project/src/Components/Users/User.js
+++ Udemy/01-project/src/Components/Users/User.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const User = props => {
+    const onDelete = item => {
+        props.onDelete(props.id);
+    };
+
     return (
-        <div style={{backgroundColor: 'white', textAlign: 'center'}}>
+        <div onClick={onDelete} style={{backgroundColor: 'white', textAlign: 'center'}}>
             Hello {props.userName} (age {props.userAge})
         </div>
     )
